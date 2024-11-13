eb01b82796939b5a20447fce749e73428f4600d7
jinliuli <jinliuli0305@gmail.com>
2024-11-13 17:51:33 +0900

    Thymeleaf

---
+++ boor-thymeleaf/src/main/java/com/test/thymeleaf/BoorThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.test.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoorThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoorThymeleafApplication.class, args);
+	}
+
+}
---
+++ boot-mybatis/src/main/java/com/test/mybatis/controller/AddressController.java
@@ -0,0 +1,33 @@
+package com.test.mybatis.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.test.mybatis.domain.AddressDTO;
+import com.test.mybatis.service.AddressService;
+
+@Controller
+public class AddressController {
+	
+	private final AddressService service;
+	
+	@Autowired
+	public AddressController (AddressService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/list.do")
+	public String list(Model model) {
+		
+		List<AddressDTO> list = service.list();
+		
+		model.addAttribute("list", list);
+		
+		
+		return "list";
+	}
+}
